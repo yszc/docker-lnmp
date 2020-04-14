a8921ad532746977dbbed5322e48bcbe28d31fe2
yumibaba <2951039279@qq.com>
2020-04-14 17:33:54 +0800

    修复mysql链接配置

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <?php
 // Docker-LNMP
 try {
-	$dbh = new PDO('mysql:host=mysql1;dbname=mysql', 'root', root);
+	$dbh = new PDO('mysql:host=mysql;dbname=mysql', 'root', root);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$dbh->exec('SET CHARACTER SET utf8');
 	$dbh = null; 	
